ae23148c9181651cc61e36d72c2208f8ed293fa0
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-06-09 00:46:45 -0400

    styling app

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,9 @@
-class Gram < ApplicationRecord
+class Gram < ActiveRecord::Base
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
+
+  mount_uploader :picture, PictureUploader
 
   belongs_to :user
-   mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609034450) do
+ActiveRecord::Schema.define(version: 20170609040555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "gram_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["gram_id"], name: "index_comments_on_gram_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "grams", force: :cascade do |t|
     t.text     "message"
